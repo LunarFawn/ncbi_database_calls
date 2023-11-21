cf9b365cd573ef9a9baf1560289680716824e245
Jennifer Pearl <jenpearl5@gmail.com>
2023-11-21 03:36:10 +0000

    backup after cmcscan working

---
+++ src/rfam_calls/scripts/get_local_cmcsan_rfam_data.py
@@ -0,0 +1,17 @@
+"""
+File to get rfam data via local cmscan search
+"""
+
+
+
+import subprocess
+import os
+
+
+def run_cmscan(db_path:str, fasta_path:str):
+    command:str = f'cmscan --noali {db_path} {fasta_path}'
+    result = os.popen(cmd=command)
+    print(result.read())
+    
+run_cmscan(db_path='/home/rnahub/rnahub/db/rfam/Rfam.cm',
+           fasta_path='/home/pearljen/repo/ncbi_database_calls/src/rfam_calls/scripts/example/example.fa')
